1b9d764b797fb5feb9bf1a9a17018445182e5986
Carlos Cuba <carlos.cuba.412@my.csun.edu>
2026-04-20 15:31:11 -0700

    Added Prototype Key Collection
    -Had A3 (where I think the key was supposed to be) implement mouselistener, creating a label for the key that disappears when clicked and goes to inventory. (**need a smaller key image)
    --learned purpose of static variables and used them to make the inventory buttons and array static for easier access among rooms.
    -Made the textbox its own class instead of inside the player data fields
    --added a label and an example w/ html tags to make text fit within the textbox

--- Project Ver 1.4/src/Player.java
+++ Project Ver 1.4/src/Player.java
@@ -1,22 +1,20 @@
-import java.awt.Color;
-import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-public class Player {
-    Inventory inventory;
+public class Player{
+    static Inventory inventory;
     //Map map;
-    JPanel textBox = new JPanel();
-    Color panelColor = Color.decode("#18230F");
-    Color borderColor = Color.decode("#27391C");
+    static TextBox textBox;
+    static String currLocation; //change with
     
     Player(){
         inventory = new Inventory();
         inventory.create();
         //map = new Map();
         //map.create(); //would also have a getMap method to get the button/panel 
-        textBox.setBounds(100,500,925,200);
-        textBox.setBackground(panelColor);
-        textBox.setBorder(BorderFactory.createLineBorder(borderColor,10));
+        textBox = new TextBox();
+        textBox.create();
+        currLocation = "SS";
+        //SS = start screen, ES = End Screen, rest are just A1, A2, etc
     }
 
     JPanel getInventory(){
@@ -30,6 +28,14 @@ public class Player {
 
     JPanel getTextBox(){
         return textBox;
+    }
+
+    static void changeCurrentLocation(String loc){
+        currLocation = loc;
+    }
+
+    static String getCurrentLocation(){
+        return currLocation;
     }
 
 }
