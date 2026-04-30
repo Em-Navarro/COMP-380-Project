67f41038af831ddd441c7c3aad852b4756020b14
Carlos Cuba <carlos.cuba.412@my.csun.edu>
2026-04-30 16:19:39 -0700

    Still trying to fix Inventory conflict

--- Version 1.6/Inventory.java
+++ Version 1.6/Inventory.java
@@ -101,8 +101,15 @@ public class Inventory extends JPanel implements ActionListener{
    }
 
    public static void useItemTwo(){
-      if (!hasItemTwo) 
+      if (!hasItemTwo)
          return;
+      if ("C2".equals(Player.getCurrentLocation())) {
+         //C2.unlockGateWithNote();
+         hasItemTwo = false;
+         slots[2].setText("Used");
+         slots[2].setEnabled(false);
+         return;
+      }
       if ("B2".equals(Player.getCurrentLocation())) {
          B2.tryUnlockGateWithCodePrompt();
       }
