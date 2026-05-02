33826ac6678f41e87c454c9055e6db83f524c3b7
Carlos Cuba <carlos.cuba.412@my.csun.edu>
2026-05-01 17:22:50 -0700

    Fixed beginning cutscene :)

--- Version 1.6/TextBox.java
+++ Version 1.6/TextBox.java
@@ -36,8 +36,6 @@ public class TextBox extends JPanel{
         label.setBounds(100,550,925,150);
         label.setFont(new Font("MV Boli",Font.PLAIN,20));
         label.setForeground(Color.white);
-        //label.setText("<html><body style='width: 675px; padding: 5px;'>This is a really long string that I am trying to see will wrap around if I put html tags around them. IDk what to type I just want to make sure that the wrapping is working so Im going to keep typing until I think I put enough words to show it working.</html>");
-        //could also use <html>Long<br>String</html> to add a break in the middle
 
         isTyping = false;
         isTypingOnCutscene = false;
@@ -106,6 +104,6 @@ public class TextBox extends JPanel{
                 }
             }
         };
-        timer.scheduleAtFixedRate(task,0,50);   
+        timer.scheduleAtFixedRate(task,100,50);   
     }
 }
