ccf50d5a1a89307873537d60afe06e06dfc442ef
Carlos Cuba <carlos.cuba.412@my.csun.edu>
2026-05-04 13:37:26 -0700

    Removing Comment in Inventory

--- Version 1.6/Inventory.java
+++ Version 1.6/Inventory.java
@@ -104,24 +104,7 @@ public class Inventory extends JPanel implements ActionListener{
    public static void useItemTwo(){
       if (!hasItemTwo)
          return;
-     /*
-      if ("C2".equals(Player.getCurrentLocation())) {
-         //C2.unlockGateWithNote();
-         hasItemTwo = false;
-         slots[2].setText("Used");
-         slots[2].setEnabled(false);
-         return;
-      }
-      if ("B2".equals(Player.getCurrentLocation())) {
-       //  B2.tryUnlockGateWithCodePrompt();
-       
-      }
-      else {
-         TextBox.writeToTextBox("Hey, I'm getting tired of constantly having to remember what the code is. Can't you make it easier to remember? Just write it on the wall or something.", null);
-      }
-      */
-     
-         TextBox.writeToTextBox("Hey, I'm getting tired of constantly having to remember what the code is. Can't you make it easier to remember? Just write it on the wall or something.", null);
+      TextBox.writeToTextBox("Hey, I'm getting tired of constantly having to remember what the code is. Can't you make it easier to remember? Just write it on the wall or something.", null);
       
    }
 
